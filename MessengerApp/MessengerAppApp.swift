7d62b1214cdf1de2183d1469b4cb70c03aa6e31e
Anthony Magana <anthonymagana0705@gmail.com>
2022-05-14 23:06:07 -0700

    Initial Commit

---
+++ MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by Anthony Magana on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
